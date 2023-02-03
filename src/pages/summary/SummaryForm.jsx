c5739f532899f7566077a0dc4255099859765a44
Michael Duberstein <dubermic@flutterint.com>
2023-02-03 17:05:18 -0500

    In Clip 46 Optional React Code: Popover, just after copying the example code

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -5,6 +5,27 @@ import Button from "react-bootstrap/Button";
 export default function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false);
 
+  import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+  import Popover from 'react-bootstrap/Popover';
+  
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Popover right</Popover.Header>
+      <Popover.Body>
+        And here's some <strong>amazing</strong> content. It's very engaging.
+        right?
+      </Popover.Body>
+    </Popover>
+  );
+  
+  const Example = () => (
+    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+      <Button variant="success">Click me to see</Button>
+    </OverlayTrigger>
+  );
+  
+  render(<Example />);
+
   const checkboxLabel = (
     <span>
       I agree to
