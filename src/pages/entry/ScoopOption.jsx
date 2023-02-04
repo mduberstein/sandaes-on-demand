c7880d6fcca5cb2030fe1a60f5c75a4552bfabb1
Michael Duberstein <dubermic@flutterint.com>
2023-02-03 22:36:01 -0500

    In Clip 54 OPTIONAL React Code: Options and ScoopOption Components, before styling ScoopOptions

---
+++ src/pages/entry/ScoopOption.jsx
@@ -0,0 +1,9 @@
+export default function ScoopOption({ name, imagePath }) {
+  return (
+    <div>
+      <img src={`http://localhost:3030/${imagePath}`} 
+        alt={`${name} scoop`}
+      />
+    </div>
+  );
+}
--- src/pages/entry/ScoopOptions.jsx
+++
@@ -1,3 +0,0 @@
-export default function ScoopOptions() {
-  return <div />;
-}
