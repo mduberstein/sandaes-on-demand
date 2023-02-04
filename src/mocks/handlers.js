629124ae2442796c5c882b0636844a422c948ae8
Michael Duberstein <dubermic@flutterint.com>
2023-02-03 23:19:30 -0500

    After Clip 55 Using  to Find Elements that populate Asynchronously

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -5,7 +5,7 @@ export const handlers = [
   rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
       ctx.json([
-        { name: 'Chocolate', imagePath: '/images/chocolate.png'},
+        { name: 'Chocolate', imagePath: '/images/chocolate.png' },
         { name: 'Vanilla', imagePath: '/images/vanilla.png' }
       ])
     );
--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -2,15 +2,15 @@ import { render, screen } from '@testing-library/react'
 
 import Options from '../Options'
 
-test ('displays image form each scoop from the server', () => {
+test ('displays image form each scoop from the server', async() => {
   render(<Options optionType="scoops"/> );
 
   // find images, name here is the altText property of the img html tag
-  const scoopImages = screen.getAllByRole('img', {name: /scoop$/i});
+  const scoopImages = await screen.findAllByRole('img', {name: /scoop$/i});
   expect(scoopImages).toHaveLength(2);
 
   // confirm alt text of images
-  const altText = scoopImages.map((element) => element.altText);
+  const altText = scoopImages.map((element) => element.alt);
   // strings and numbers - immutable types use .toBe matcher, while
   // mutable types - arrays and objects use .toEqual matcher
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
