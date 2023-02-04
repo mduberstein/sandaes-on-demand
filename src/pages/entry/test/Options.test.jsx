6e06fd0517815febc81fa77e1802226e202aa463
Michael Duberstein <dubermic@flutterint.com>
2023-02-03 23:43:04 -0500

    After Clip 56 Code Quiz! Topping Options from Server

--- src/pages/entry/test/Options.test.jsx
+++ src/pages/entry/test/Options.test.jsx
@@ -16,3 +16,16 @@ test ('displays image form each scoop from the server', async() => {
   expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
 })
 
+test ('displays image form each topping from the server', async() => {
+  render(<Options optionType="toppings"/> );
+
+  // find images, name here is the altText property of the img html tag
+  const scoopImages = await screen.findAllByRole('img', {name: /topping$/i});
+  expect(scoopImages).toHaveLength(3);
+
+  // confirm alt text of images
+  const altText = scoopImages.map((element) => element.alt);
+  // strings and numbers - immutable types use .toBe matcher, while
+  // mutable types - arrays and objects use .toEqual matcher
+  expect(altText).toEqual(['Cherries topping', 'M&Ms topping', 'Hot fudge topping']);
+})
